6815fdd0a8842d2eafa0698aa79b8035639f345b
Kang Yun Seo <dbstj16@sookmyung.ac.kr>
2022-04-10 18:23:24 +0900

    [FEAT] password eye 기능 추가

--- Instagram_clone/Instagram_clone/ViewController.swift
+++ Instagram_clone/Instagram_clone/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var passwordHideButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        setEnableButton()
+    }
+    
     @IBAction func signinButton(_ sender: Any) {
         guard let signinVC = self.storyboard?.instantiateViewController(withIdentifier: "finishViewController") as? finishViewController else {return}
         signinVC.modalPresentationStyle = .fullScreen
@@ -23,11 +33,6 @@ class ViewController: UIViewController {
         
         self.present(signinVC, animated: true, completion: nil)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
 
     @IBAction func signupButton(_ sender: Any) {
@@ -36,5 +41,27 @@ class ViewController: UIViewController {
         
        self.navigationController?.pushViewController(signupVC, animated: true)
     }
+    
+    @IBAction func passwordHideButton(_ sender: Any) {
+        if passwordTextField.isSecureTextEntry {
+            passwordHideButton.setImage(UIImage(named: "passwordHideEye"), for: .normal)
+            passwordTextField.isSecureTextEntry = false
+        }
+        else {
+            passwordHideButton.setImage(UIImage(named: "passwordShownEye"), for: .normal)
+            passwordTextField.isSecureTextEntry = true
+        }
+    }
+    
+//    private func setEnableButton() {
+//        if (idTextField.hasText && passwordTextField.hasText) == true {
+//            loginButton.isEnabled = true
+//        }
+//        else {
+//            loginButton.isEnabled = false
+//        }
+//    }
+    
+    
 }
 
--- Instagram_clone/Instagram_clone/passwordViewController.swift
+++ Instagram_clone/Instagram_clone/passwordViewController.swift
@@ -11,6 +11,8 @@ class passwordViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var passwordHideButton: UIButton!
+    
     var userId: String?
     
     override func viewDidLoad() {
@@ -19,7 +21,17 @@ class passwordViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func passwordHideButton(_ sender: Any) {
+        if passwordTextField.isSecureTextEntry {
+            passwordHideButton.setImage(UIImage(named: "passwordHideEye"), for: .normal)
+            passwordTextField.isSecureTextEntry = false
+        }
+        else {
+            passwordHideButton.setImage(UIImage(named: "passwordShownEye"), for: .normal)
+            passwordTextField.isSecureTextEntry = true
+        }
+    }
+    
     @IBAction func loginButton(_ sender: Any) {
         guard let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "finishViewController") as? finishViewController else {return}
         self.navigationController?.pushViewController(loginVC, animated: true)
@@ -31,6 +43,15 @@ class passwordViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
 
     
+
     
+//    private func setButtonEnable() {
+//        if passwordTextField.state.isEmpty == true {
+//            loginButton.isEnabled = false
+//        }
+//        else {
+//            loginButton.isEnabled = true
+//        }
+
 
 }
