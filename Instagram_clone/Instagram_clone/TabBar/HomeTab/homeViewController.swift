2b2f1d420579e5e12bbe2692a11089a38d25ff1a
Kang Yun Seo <dbstj16@sookmyung.ac.kr>
2022-05-06 03:15:26 +0900

    [FEAT] 스토리 컬렉션 뷰 구현

---
+++ Instagram_clone/Instagram_clone/TabBar/HomeTab/homeViewController.swift
@@ -0,0 +1,68 @@
+//
+//  homeViewController.swift
+//  Instagram_clone
+//
+//  Created by 강윤서 on 2022/05/06.
+//
+
+import UIKit
+
+class homeViewController: UIViewController {
+
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let nib = UINib(nibName: StoryCollectionViewCell.identifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: StoryCollectionViewCell.identifier)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+    }
+}
+
+extension homeViewController: UICollectionViewDelegate {
+    
+}
+
+extension homeViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return StoryDataModel.sampleData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StoryCollectionViewCell.identifier, for: indexPath) as? StoryCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.setData(storyData: StoryDataModel.sampleData[indexPath.row])
+        return cell
+    }
+}
+
+extension homeViewController: UICollectionViewDelegateFlowLayout{
+    // 각 cell의 아이템 크기
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        
+        let cellWidth = width * (58/375)
+        let cellHeight = cellWidth * (72/58)
+        
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+    // cell에서 content 외부에 존재하는 inset의 크기를 결정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 6, left: 5, bottom: 4, right: 5)
+    }
+    
+    // cell 좌우간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+}
