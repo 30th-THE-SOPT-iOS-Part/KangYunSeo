38ce87a2de52fa34f3fb4879b8379b4c736f58bb
Kang Yun Seo <dbstj16@sookmyung.ac.kr>
2022-05-06 04:10:42 +0900

    [ADD] 피드 이미지 에셋 추가

---
+++ Instagram_clone/Instagram_clone/TabBar/HomeTab/TableView/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Instagram_clone
+//
+//  Created by 강윤서 on 2022/05/06.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
