0422f1b3af475ad140d0296af30e2201b44b7372
Kang Yun Seo <dbstj16@sookmyung.ac.kr>
2022-05-08 03:57:29 +0900

    ➕[ADD] APIConstants 파일 생성

---
+++ 4th_seminar/4th_seminar/Network/NetworkResult.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkResult.swift
+//  4th_seminar
+//
+//  Created by 강윤서 on 2022/05/08.
+//
+
+enum NetworkResult<T> {
+    case success(T)             // 서버 통신 성공
+    case requestErr(T)          // 요청 에러 발생
+    case pathErr                // 경로 에러
+    case serverErr              // 서버의 내부 에러
+    case networkFail            // 네트워크 연결 실패
+}
