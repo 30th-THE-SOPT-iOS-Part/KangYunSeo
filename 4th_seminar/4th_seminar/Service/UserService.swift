4825fe4bb85998754d63309986b4cc9f588c1d2c
Kang Yun Seo <dbstj16@sookmyung.ac.kr>
2022-05-08 04:25:00 +0900

    ➕[ADD] UserService 파일 추가

--- 4th_seminar/4th_seminar/Service/UserService.swift
+++ 4th_seminar/4th_seminar/Service/UserService.swift
@@ -8,8 +8,19 @@
 import Foundation
 import Alamofire
 
+/*
+ 서비스 코드
+ 
+ - Request
+    ㄴ 요청서를 작성하기 위한 준비물을 준비하고
+    ㄴ 그것들을 적절하게 사용해서 서버에게 요청을 하게 됩니다.
+ - Response
+ */
+
 class UserService {
-    static let shared = UserService()       // 싱글톤 객체
+    // 싱글톤 변수(Singleton)
+    static let shared = UserService()
+    
     private init() {}
     
     // 로그인 통신 시 사용할 함수
@@ -17,34 +28,35 @@ class UserService {
                email: String,
                password: String,
                completion: @escaping (NetworkResult<Any>) -> Void)
-    
     {
-        // completion 클로저를 @escaping closure로 정의
+        // completion 클로저를 @escaping closure로 정의합니다.
         
-        let url = APIConstants.loginURL     // 통신할 API 주소
-        let header: HTTPHeaders = ["Content=Type" : "application/json"]     // 요청 헤더
-        let body: Parameters = [            // 요청 바디
+        let url = APIConstants.loginURL                                     // URL
+        let header: HTTPHeaders = ["Content-Type" : "application/json"]     // HTTP Headers
+        let body: Parameters = [                                            // HTTP body
             "name": name,
             "email": email,
             "password": password
         ]
         
-        // 요청서 - 통신할 API 주소, HTTP method, 요청 방식, 인코딩 방식, 요청 헤더 순
-        // Request 생성
+        // 요청서
+        // Request 생성 - 통신할 API 주소, HTTP method, 요청 방식, 인코딩 방식, 요청 헤더 순
         let dataRequest = AF.request(url,
-                                     method: .post,
-                                     parameters: body,
-                                     encoding: JSONEncoding.default,
-                                     headers: header)
+                                    method: .post,
+                                    parameters: body,
+                                    encoding: JSONEncoding.default,
+                                    headers: header)
         
         // 서버 통신 시작 - Request 시작
         dataRequest.responseData { response in
-            // 요청(Request)를 하고 넘겨받은 응답의 결과를 가지고 성공 또는 실패 분기 처리를 한다.
+            // 요청(Request)를 하고 넘겨받은 응답의 결과를 가지고 성공 또는 실패 분기 처리를 합니다.
             switch response.result {
             case .success:
-                // 성공시, 상태코드(Status Code)와 값(Value)가 넘어옴
-                guard let statusCode = response.response?.statusCode else {return}
-                guard let value = response.value else {return}
+                // 성공 시에는 상태코드(Status Code)와 값(Value)이 넘어옴
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let value = response.value else { return }
+                
+                // 해당 응답을 가지고 case 분기처리를 합니다. (200, 400, 500인지 - 200: 성공을 해서 데이터를 잘 받았는지 확인합니다.)
                 let networkResult = self.judgeStatus(by: statusCode, value)
                 completion(networkResult)
             
@@ -59,21 +71,20 @@ class UserService {
     private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
         switch statusCode {
         // 성공 시에는 넘겨받은 데이터를 decode(해독)하는 함수를 호출합니다.
-        case 200: return isValidData(data: data)
+        case 200: return isVaildData(data: data)
         case 400: return .pathErr
         case 500: return .serverErr
         default: return .networkFail
         }
     }
     
-    // 넘겨받은 데이터를 decode하는 함수
+    // 성공 시 넘겨받은 데이터를 decode하는 함수입니다.
     // 이 때 우리가 codable을 채택해서 만들어 놓은 구조체 형식의 데이터 모델을 사용합니다.
-    private func isValidData(data: Data) -> NetworkResult<Any> {
+    private func isVaildData(data: Data) -> NetworkResult<Any> {
         let decoder = JSONDecoder()
         guard let decodedData = try? decoder.decode(LoginResponse.self, from: data)
-        else { return .pathErr}
+        else { return .pathErr }
         
-        return .success(decodedData.data as Any)
+        return .success(decodedData as Any)
     }
 }
-
