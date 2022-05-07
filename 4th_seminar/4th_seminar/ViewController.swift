3d7b66ebf988586e29753ee60ae85b0157cce15e
Kang Yun Seo <dbstj16@sookmyung.ac.kr>
2022-05-07 16:21:28 +0900

    [FEAT] 컬렉션 뷰 스크롤 숨기기

---
+++ 4th_seminar/4th_seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4th_seminar
+//
+//  Created by 강윤서 on 2022/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
