e44e3056e0c84559dc7f57b682eb80593c81ee72
Kang Yun Seo <dbstj16@sookmyung.ac.kr>
2022-05-08 04:16:18 +0900

    [FEAT] 로그인 화면 구현

--- 4th_seminar/4th_seminar/ViewController.swift
+++ 4th_seminar/4th_seminar/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
