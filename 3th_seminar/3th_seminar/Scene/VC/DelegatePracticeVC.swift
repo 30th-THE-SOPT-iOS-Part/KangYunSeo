cd1611a3d51be32e3b69d1eb504e82158891e2b8
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-08 23:19:21 +0900

    [FEAT] DelegatePractice 실습 환경 세팅

---
+++ 3th_seminar/3th_seminar/Scene/VC/DelegatePracticeVC.swift
@@ -0,0 +1,26 @@
+//
+//  DelegatePracticeVC.swift
+//  3th_seminar
+//
+//  Created by 강윤서 on 2022/06/08.
+//
+
+import UIKit
+
+class DelegatePracticeVC: UIViewController {
+
+    //MARK: - Vars & Lets
+    
+    //MARK: - UI Component Part
+    @IBOutlet weak var textField: UITextField!
+    
+    //MARK: - Life Cycle Part
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+}
+
+//MARK: - Extension Part
