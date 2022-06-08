4c077d3d627e9bd5feaa4fae620340b77a0cd059
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-08 22:29:49 +0900

    [FEAT] #9 ScrollView 실습

---
+++ 3th_seminar/3th_seminar/Scene/VC/ScrollVC.swift
@@ -0,0 +1,23 @@
+//
+//  ScrollVC.swift
+//  3th_seminar
+//
+//  Created by 강윤서 on 2022/06/08.
+//
+
+import UIKit
+
+class ScrollVC: UIViewController {
+
+    //MARK: - Vars & Lets Part
+    
+    //MARK: - UI Component Part
+    
+    //MARK: - Life Cycle Part
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
