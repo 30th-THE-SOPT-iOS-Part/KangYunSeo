20a1f23e7b2976817a043dbdc2a70dc54192f522
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-09 01:53:13 +0900

    [ADD] 첫 번째 뷰에 컴포넌트 추가

---
+++ 3th_seminar/3th_seminar/Scene/VC/DelegatePracticeSecondVC.swift
@@ -0,0 +1,29 @@
+//
+//  DelegatePracticeSecondVC.swift
+//  3th_seminar
+//
+//  Created by 강윤서 on 2022/06/09.
+//
+
+import UIKit
+
+class DelegatePracticeSecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
