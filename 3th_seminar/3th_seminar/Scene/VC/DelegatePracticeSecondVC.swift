97e8441c73b9bd2b37e26af4a6c90b54f8708e94
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-09 02:36:01 +0900

    [ADD] 프로토콜 생성

--- 3th_seminar/3th_seminar/Scene/VC/DelegatePracticeSecondVC.swift
+++ 3th_seminar/3th_seminar/Scene/VC/DelegatePracticeSecondVC.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
+//MARK: - Protocol Part
+// 구현해야하는 함수 원형만 작성
+protocol DelegatePracticeSecondVCDelegate {
+    func sendData(data: String)
+}
+
 class DelegatePracticeSecondVC: UIViewController {
 
     //MARK: - Vars & Lets Part
-    
+    //위에서 만든 프로토콜형의 delegate 프로퍼티 생성
+    var delegate: DelegatePracticeSecondVCDelegate?
     
     //MARK: - UIComponent Part
     
