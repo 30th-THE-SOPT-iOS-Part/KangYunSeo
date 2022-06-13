f4ac94e59bb87c89fd8e4dc279048d7b9a9a1589
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-13 23:59:13 +0900

    [ADD] 테이블뷰 실습 VC파일 추가

---
+++ 3th_seminar/3th_seminar/Scene/VC/TableViewVC.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewVC.swift
+//  3th_seminar
+//
+//  Created by 강윤서 on 2022/06/13.
+//
+
+import UIKit
+
+class TableViewVC: UIViewController {
+
+    //MARK: - Vars & Lets Part
+    
+    //MARK: - UIComponent Part
+    
+    
+    //MARK: - Life Cycle Part
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - IBAction Part
+    
+}
