c814b1d891f869a40083bdc1c0434697d32c3fa0
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-14 18:19:40 +0900

    [ADD] 더미 데이터 추가

--- 3th_seminar/3th_seminar/Scene/DataModel/MusicDataModel.swift
+++ 3th_seminar/3th_seminar/Scene/DataModel/MusicDataModel.swift
@@ -30,5 +30,8 @@ extension MusicDataModel {
         MusicDataModel(albumCoverImageName: "albumCover06", musicTitle: "널 너무 모르고", author: "헤이즈(Heize)", playTime: "3:09"),
         MusicDataModel(albumCoverImageName: "albumCover07", musicTitle: "스물다섯, 스물하나", author: "자우림", playTime: "4:44"),
         MusicDataModel(albumCoverImageName: "albumCover08", musicTitle: "Joker(feat. JAMIE)", author: "BIG Naughty(서동현)", playTime: "3:31"),
+        MusicDataModel(albumCoverImageName: "albumCover01", musicTitle: "긴 밤(feat. GIRIBOY)", author: "Seori", playTime: "3:31"),
+        MusicDataModel(albumCoverImageName: "albumCover02", musicTitle: "Runnign Through The Night", author: "서리", playTime: "3:38"),
+        MusicDataModel(albumCoverImageName: "albumCover03", musicTitle: "instagram", author: "DEAN(딘)", playTime: "4:15")
     ]
 }
