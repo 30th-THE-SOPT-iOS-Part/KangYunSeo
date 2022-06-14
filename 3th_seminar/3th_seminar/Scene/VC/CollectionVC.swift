73a05eff674ca798f4e913d110445847fc2812d4
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-15 00:21:26 +0900

    [FEAT] 컬렉션 뷰 스토리보드, VC 수정

---
+++ 3th_seminar/3th_seminar/Scene/VC/CollectionVC.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionVC.swift
+//  3th_seminar
+//
+//  Created by 강윤서 on 2022/06/15.
+//
+
+import UIKit
+
+class CollectionVC: UIViewController {
+
+    //MARK: - Vars & Lets Part
+    
+    //MARK: - UIComponent Part
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    //MARK: - Life Cycle Part
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+
+//MARK: - Extension Part
