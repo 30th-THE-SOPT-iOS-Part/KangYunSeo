e6c73c5fba20db3f5da8e41a4f599b68f5a8eca8
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-15 00:33:02 +0900

    [FEAT] 컬렉션 뷰 실습 데이터 모델 생성

---
+++ 3th_seminar/3th_seminar/Scene/DataModel/AlbumDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumDataModel.swift
+//  3th_seminar
+//
+//  Created by 강윤서 on 2022/06/15.
+//
+
+import Foundation
+
+struct AlbumDataModel {
+    let albumImageName: String
+    let albumTitle: String
+    let albumSinger: String
+}
