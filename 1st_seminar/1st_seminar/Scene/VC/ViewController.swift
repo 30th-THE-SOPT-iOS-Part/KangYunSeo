896e96e368b9d17cd36eae4f5e78f1ec0b940fc8
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-15 13:25:16 +0900

    [FEAT] 버튼 커스텀 및 버튼 클릭에 따른 라벨 내용 변경

--- 1st_seminar/1st_seminar/Scene/VC/ViewController.swift
+++ 1st_seminar/1st_seminar/Scene/VC/ViewController.swift
@@ -14,18 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var UIView: UIView!
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var finishButton: UIButton!
     
     
     //MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonCustom()
     }
 
     //MARK: - IBAction Part
     @IBAction func finishButtonDidTap(_ sender: Any) {
+        if messageLabel.text == "도착한 메세지가 없어요!" {
+            messageLabel.text = "새로운 메세지가 도착했어요!✉️"
+            messageLabel.textColor = UIColor.purple
+        }
+        else {
+            messageLabel.text = "도착한 메세지가 없어요!"
+            messageLabel.textColor = UIColor.black
+        }
     }
     
-
+    //MARK: - Functions Part
+    func buttonCustom() {
+        finishButton.layer.borderWidth = 1
+        finishButton.layer.borderColor = UIColor.black.cgColor
+        finishButton.layer.cornerRadius = 10
+    }
 }
 
