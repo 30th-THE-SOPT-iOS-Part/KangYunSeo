94491e228b8cd4330fb6478539dcfef579d0bad1
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-15 13:07:17 +0900

    [FEAT] 컴포넌트 연결

--- 1st_seminar/1st_seminar/Scene/VC/ViewController.swift
+++ 1st_seminar/1st_seminar/Scene/VC/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - UIComponent Part
+    @IBOutlet weak var UIImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var UIView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var finishButtonDidTap: UIButton!
+    
+    
+    //MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
