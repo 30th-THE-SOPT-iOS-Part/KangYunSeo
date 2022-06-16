a6f3506cc775efd37ab0fd13f1a4e1beb77d1603
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-17 00:54:49 +0900

    [FEAT] setTabBarController 함수 작성

--- Seminar/2nd_seminar/2nd_seminar/Scene/VC/TabBarWithCode/TabBarWithCodeController.swift
+++ Seminar/2nd_seminar/2nd_seminar/Scene/VC/TabBarWithCode/TabBarWithCodeController.swift
@@ -12,7 +12,23 @@ class TabBarWithCodeController: UITabBarController {
     //MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setTabBarController()
 
+    }
+    
+    //MARK: - Function Part
+    func setTabBarController() {
+        //1. 연결할 스토리보드 인스턴스 생성
+        guard let blueTabVC = self.storyboard?.instantiateViewController(withIdentifier: "BlueTabVC"),
+              let greenTabVC = self.storyboard?.instantiateViewController(withIdentifier: "GreenTabVC") else { return }
+        
+        //2. 탭 바 아이템 생성
+        blueTabVC.tabBarItem = UITabBarItem(title: "home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        greenTabVC.tabBarItem = UITabBarItem(title: "profile", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
+        
+        //3. 뷰 컨트롤러 연결하기
+        setViewControllers([blueTabVC, greenTabVC], animated: true)
     }
     
 
