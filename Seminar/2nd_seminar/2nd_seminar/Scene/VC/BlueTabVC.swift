dd6ae1bf8303ed801c7d95704df7f2993258ad0b
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-17 00:30:19 +0900

    [ADD] 코드베이스 실습을 위한 파일 생성

---
+++ Seminar/2nd_seminar/2nd_seminar/Scene/VC/BlueTabVC.swift
@@ -0,0 +1,19 @@
+//
+//  BlueTabVC.swift
+//  2nd_seminar
+//
+//  Created by 강윤서 on 2022/06/17.
+//
+
+import UIKit
+
+class BlueTabVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Seminar/2nd_seminar/2nd_seminar/Scene/VC/TabBarWithCodeController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarWithCodeController.swift
+//  2nd_seminar
+//
+//  Created by 강윤서 on 2022/06/17.
+//
+
+import UIKit
+
+class TabBarWithCodeController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
