3ee77313f2d31dbb94d7ac7db1d1ea348ef58b0e
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-16 20:20:01 +0900

    [ADD] 네비게이션 실습을 위한 파일 생성

---
+++ 1st_seminar/1st_seminar/Scene/VC/NavigationFirstVC.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationFirstVC.swift
+//  1st_seminar
+//
+//  Created by 강윤서 on 2022/06/16.
+//
+
+import UIKit
+
+class NavigationFirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - IBAction Part
+    @IBAction func pushButtonDidTap(_ sender: Any) {
+    }
+    
+}
---
+++ 1st_seminar/1st_seminar/Scene/VC/NavigationSecondVC.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationSecondVC.swift
+//  1st_seminar
+//
+//  Created by 강윤서 on 2022/06/16.
+//
+
+import UIKit
+
+class NavigationSecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
