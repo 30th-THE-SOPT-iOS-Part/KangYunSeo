43109e1d8f996f4d06bfec84cd113644c28ae312
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-21 02:12:57 +0900

    [ADD] 컬렉션뷰셀을 담을 테이블뷰셀 파일 생성

---
+++ Instagram_clone/Instagram_clone/TabBar/HomeTab/TableView/Cell/storyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  storyTableViewCell.swift
+//  Instagram_clone
+//
+//  Created by 강윤서 on 2022/06/21.
+//
+
+import UIKit
+
+class storyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
