125b958bd496efed05f81bedf6e2f13ffb34044e
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-21 02:28:14 +0900

    [FEAT] 셀 등록, 셀 identifier 선언, extension 선언

--- Instagram_clone/Instagram_clone/TabBar/HomeTab/TableView/Cell/storyTableViewCell.swift
+++ Instagram_clone/Instagram_clone/TabBar/HomeTab/TableView/Cell/storyTableViewCell.swift
@@ -9,15 +9,47 @@ import UIKit
 
 class storyTableViewCell: UITableViewCell {
 
+    //MARK: - Vars & Lets
+    static let identifier = "storyTableViewCell"
+    
+    //MARK: - UIComponent
+    @IBOutlet var storyCollectionView: UICollectionView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        let storyNib = UINib(nibName: StoryCollectionViewCell.identifier, bundle: nil)
+        storyCollectionView.register(storyNib, forCellWithReuseIdentifier: StoryCollectionViewCell.identifier)
+        
+        storyCollectionView.delegate = self
+        storyCollectionView.dataSource = self
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
+    
+}
+
+//MARK: - Extension Part
+extension storyTableViewCell: UICollectionViewDelegate {
+    
+}
+
+extension storyTableViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+extension storyTableViewCell: UICollectionViewDelegateFlowLayout {
     
 }
