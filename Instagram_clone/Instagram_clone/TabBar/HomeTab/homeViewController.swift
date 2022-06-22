d1d22df3a6ce5a59165afb19c7e097705d14fa9f
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-23 04:05:50 +0900

    [FEAT] UITableViewDataSource 작성

--- Instagram_clone/Instagram_clone/TabBar/HomeTab/homeViewController.swift
+++ Instagram_clone/Instagram_clone/TabBar/HomeTab/homeViewController.swift
@@ -35,3 +35,36 @@ extension homeViewController: UITableViewDelegate {
         return indexPath.section == 0 ? 84 : 488
     }
 }
+
+extension homeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return FeedDataModel.sampleData.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: storyTableViewCell.identifier, for: indexPath) as? storyTableViewCell else {
+                return UITableViewCell() }
+            return cell
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier, for: indexPath) as? TableViewCell else {
+                return UITableViewCell() }
+            cell.setData(FeedDataModel.sampleData[indexPath.row])
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+}
